3402f86d265567bdcdeff9a2305d60188929bd44
Yohansel Mieses <miesesyohansel@gmail.com>
2025-11-03 19:55:35 -0400

    wacakaaa

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("MIra, este es un cambio mio. Este es un cambio, desde esta PC");
         Console.WriteLine("AY MALDITO LOCO");
         Console.WriteLine("PUERCOS EVOOOOOOOOOOOOOOO MALDITO LOCO");
+        Console.WriteLine("lanzafeugoo EVOOOOOOOOOOOOOOO MALDITO LOCO");
     }
 }
