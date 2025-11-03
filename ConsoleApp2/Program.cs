10dad2ed3cfbb37dc1945f8587963740609656ee
EP <pinedajaja0@gmail.com>
2025-11-03 19:43:29 -0400

    PUERCOS EVO AY AY YA

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,5 +6,6 @@ class Program
     {
         Console.WriteLine("MIra, este es un cambio mio. Este es un cambio, desde esta PC");
         Console.WriteLine("AY MALDITO LOCO");
+        Console.WriteLine("PUERCOS EVOOOOOOOOOOOOOOO MALDITO LOCO");
     }
 }
