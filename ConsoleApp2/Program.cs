014b96fd86d7364d1583ed79d90223a60b520da9
EP <pinedajaja0@gmail.com>
2025-11-03 19:07:05 -0400

    u nuevo commit jeje

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("MIra, este es un cambio mio. Este es un cambio, desde esta PC");
+        Console.WriteLine("AY MALDITO LOCO");
     }
 }
